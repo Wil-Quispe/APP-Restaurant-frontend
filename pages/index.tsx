cf97eacd921d8b4985d5b5f5f95b813ab2b72a9b
@Wil <emma.wilinston@gmail.com>
2021-11-08 18:13:23 -0500

    el grid del feet listo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,11 @@ const Home = () => {
       <Navbar />
       <div className="px-2 grid grid-cols-6">
         <Sidebar />
-        <div className="bg-blue-400 col-span-5">main</div>
+        <div className="bg-blue-400 col-span-5 grid grid-cols-3 justify-items-center">
+          <h1>ho</h1>
+          <h1>ho</h1>
+          <h1>ho</h1>
+        </div>
       </div>
     </div>
   )
