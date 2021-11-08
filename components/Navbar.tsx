b6df63e7a39b52d63ab92f77f14c0bfa4847d8b3
@Wil <emma.wilinston@gmail.com>
2021-11-08 17:29:34 -0500

    mejora font

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <h2 className="text-green-400">Mi Super Logo</h2>
       </div>
       <div>
-        <h1>Martes y Miercoles</h1>
+        <h1 className="text-2xl font-bold">Martes y Miercoles</h1>
       </div>
       <div>
         <span className="bg-green-400 text-white mr-4 py-2 px-5 rounded-lg">
