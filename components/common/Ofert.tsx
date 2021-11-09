624cef76f0aacb320117aa6c83f1a4eddf67cf5d
@Wil <emma.wilinston@gmail.com>
2021-11-09 14:57:25 -0500

    mejora curso y el grid de productos

--- components/common/Ofert.tsx
+++ components/common/Ofert.tsx
@@ -1,7 +1,7 @@
 const Ofert = ({ ofert }: { ofert: string }) => {
   return (
-    <div className="bg-white rounded-lg py-3 mb-3 w-11/12 text-center m-auto">
-      <span>{ofert}</span>
+    <div className="cursor-pointer bg-white rounded-lg py-3 mb-3 w-11/12 text-center m-auto">
+      <a className=" border-b border-green-400">{ofert}</a>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Navbar />
       <div className="px-2 grid grid-cols-6 mt-16">
         <Sidebar />
-        <div className="col-span-5 grid grid-cols-3 justify-items-center px-52 gap-y-8">
+        <div className="col-span-5 grid grid-cols-4 justify-items-center px-20 gap-y-8">
           <Card />
           <Card />
           <Card />
