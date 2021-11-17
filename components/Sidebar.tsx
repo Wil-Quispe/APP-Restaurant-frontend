e286c09444be07bdd690e6fed73fa2cf02b0f476
@Wil <emma.wilinston@gmail.com>
2021-11-17 11:25:26 -0500

    arreglo pequeños defectos

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
       <div className="bg-green-300 rounded-lg p-3 mb-3">
         <h1 className="text-center text-white">Ofertas</h1>
       </div>
-      <div className="mx-5 flex flex-col sm:flex-row md:flex-row lg:flex-col">
+      <div className="mx-5 flex flex-col sm:flex-row md:flex-row lg:flex-col lg:mx-2">
         <Ofert ofert="Ofertas solo Hoy!" />
         <Ofert ofert="Ofertas en Deliverys!" />
       </div>
--- components/common/Ofert.tsx
+++ components/common/Ofert.tsx
@@ -1,6 +1,6 @@
 const Ofert = ({ ofert }: { ofert: string }) => {
   return (
-    <div className="cursor-pointer bg-white rounded-lg py-3 mb-3 w-11/12 text-center m-auto">
+    <div className="cursor-pointer bg-white rounded-lg py-3 mb-3 w-full text-center m-auto sm:mr-2 md:mr-2">
       <a className=" border-b border-green-400">{ofert}</a>
     </div>
   )
